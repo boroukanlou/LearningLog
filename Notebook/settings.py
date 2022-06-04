95ed0cd8cf7740ea8003ae0c9d5f4ae511f74bad
shahin <kaghaz02@gmail.com>
2022-06-04 20:55:06 +0430

    ready to run on the heroku server

--- Notebook/settings.py
+++ Notebook/settings.py
@@ -131,3 +131,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'users:login'
+
+
+# Heroku Settings
+
+import django_heroku
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Notebook.wsgi --log-file -
